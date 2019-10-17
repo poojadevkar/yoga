a94d669c9e19f4cdc794cbde5f4aa4e03e23f47c
Pratik Bali <pratibal@amdocs.com>
2019-10-17 12:32:18 +0530

    bug fix

--- src/app/webapp/layout/login/login.component.ts
+++ src/app/webapp/layout/login/login.component.ts
@@ -18,7 +18,7 @@ export class LoginComponent implements OnInit, AfterViewInit {
   username: string;
   credentials: any;
   account: any;
-  prod = true;
+  prod = false;
 
   constructor(
     private renderer: Renderer,
